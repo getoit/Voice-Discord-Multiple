90a1b6e3b0a2ce82c449020a279c113e4cf3fb3f
getoit <unlibot002@gmail.com>
2025-03-23 21:07:25 +0700

    Version 1.0.0

--- setup/config.js
+++ setup/config.js
@@ -1,29 +1,22 @@
-// Configuration object containing various settings for the application
 const config = {
-  // Setting your prefix for commands
-  prefix: "-",  // Prefix for commands
-  // Allowing only specific users to use the bot
-  AutoWakeupJockie: false, // Enable auto wakeup functionality
-
+  prefix: "-",
+  AutoWakeupJockie: false,
   allowedUsers: [
-    "ownerID_1", // Replace with your Discord user ID 
-    "ownerID_99"  // You can add more user IDs here
+    "ownerID_1",
+    "ownerID_99"
   ],
-  // Setting the voice channel ID to connect to
   VC: {
-    channelId: "channelId", // Replace with your voice channel ID
-    selfMute: false,   // Mute the bot in the voice channel
-    selfDeaf: false,    // Deaf the bot in the voice channel
-    stream: false   // Stream audio from the voice channel
+    channelId: "channelId",
+    selfMute: false,
+    selfDeaf: false,
+    stream: false
   },
- // Setting leveling system roles (like Probot)
   levelingRole: {
-    levelingspamSet: false,     // Enable leveling spam
-    spamchannelId: "1347790044567437373",       // Replace with your spam channel ID
-    spamContent: "_ _",        // Content to be sent in the spam channel 
-    spamInterval: 50000        // Interval for sending spam messages (in milliseconds)
+    levelingspamSet: false,
+    spamchannelId: "1347790044567437373",
+    spamContent: "_ _",
+    spamInterval: 50000
   }
 };
 
-// Export the configuration object
 module.exports = config;
