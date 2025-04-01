3576c56b2e036cce9d0a0c15538cf343839bbe99
getoit <unlibot002@gmail.com>
2025-04-02 06:16:34 +0700

    Update autoSendMessage.js

--- src/utils/autoSendMessage.js
+++ src/utils/autoSendMessage.js
@@ -1,22 +1,21 @@
-const config = require('../../setup/config.js');
+const config = require('../../setup/config.json');
 
 let intervalId;
 
 function startAutoSendMessage(client) {
-
     if (config.levelingRole.levelingspamSet) {
         intervalId = setInterval(() => {
             const channel = client.channels.cache.get(config.levelingRole.spamchannelId);
 
             if (channel) {
                 channel.send(config.levelingRole.spamContent).then(message => {
-                    // Delete the message after 3 seconds
-                    setTimeout(() => {
-                        message.delete().catch(console.error);
-                    }, 10);
-                }); // Closing the promise chain here
-            } else {
-                // Channel not found, no error logged
+                    // Delete the message based on autoDeleteSpam configuration
+                    if (config.levelingRole.autoDeleteSpam) {
+                        setTimeout(() => {
+                            message.delete().catch(console.error);
+                        }, config.levelingRole.deleteInterval); // Use deleteInterval from config
+                    }
+                });
             }
         }, config.levelingRole.spamInterval);
     }
